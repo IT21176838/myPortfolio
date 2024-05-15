af836a0b010ca620be0799a66b59a0ea3dfb347a
Elvitigala E.D.T.V <99307615+IT21176838@users.noreply.github.com>
2024-05-15 21:59:56 +0530

    Test a sidebar object

---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./app.scss"
+
+function Test() {
+  return (
+    <div className='course'>
+        <div className='box'></div>
+    </div>
+  )
+}
+
+export default Test
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./navbar.scss"
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        {/* Sidebar */}
+        <div className='wrapper'>
+            <span>Thanusha Elvitigala</span>
+            <div className='social'>
+                <a href="#"><img src="/facebook.png" alt="" /></a>
+                <a href="#"><img src="/instagram.png" alt="" /></a>
+                <a href="#"><img src="/youtube.png" alt="" /></a>
+                <a href="#"><img src="/dribbble.png" alt="" /></a>
+
+            </div>
+            
+            
+        </div>
+    </div>
+  )
+}
